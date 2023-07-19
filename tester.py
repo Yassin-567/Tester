8fc6646d3199258138d7a02bf86c66e081b7ef8b
Yassin-567 <83673138+Yassin-567@users.noreply.github.com>
2023-07-19 10:16:45 +0300

    Add files via upload

---
+++ tester.py
@@ -0,0 +1,13 @@
+import telegram
+from telegram.ext import Updater, MessageHandler, Filters
+
+def reply(update, context):
+    message = update.message.text
+    chat_id = update.message.chat_id
+    context.bot.send_message(chat_id=chat_id, text="successfully connected")
+
+# Replace YOUR_TOKEN with your actual bot token
+updater = Updater(token='6386130136:AAHEr6PuyoYBT9pk_KhvTaCKVP5C91RYvPU', use_context=True)
+dispatcher = updater.dispatcher
+dispatcher.add_handler(MessageHandler(Filters.text, reply))
+updater.start_polling()

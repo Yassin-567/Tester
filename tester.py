7824ff6af8061dd9283007fc443b3e23f76efeb6
Yassin-567 <83673138+Yassin-567@users.noreply.github.com>
2023-07-21 11:26:49 +0300

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,10 +1,20 @@
 import telegram
 from telegram.ext import Updater, MessageHandler, Filters
+from flask import Flask, request
+
+app = Flask(__name__)
 
 def reply(update, context):
     message = update.message.text
     chat_id = update.message.chat_id
     context.bot.send_message(chat_id=chat_id, text="successfully connected")
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    json_string = request.get_data().decode('UTF-8')
+    update = telegram.Update.de_json(json_string)
+    dispatcher.process_update(update)
+    return 'OK'
 
 # Replace YOUR_TOKEN with your actual bot token
 updater = Updater(token='6386130136:AAEsVZqx0eRWUE3x9HlXxM8WzutfeJD_SoA', use_context=True)
